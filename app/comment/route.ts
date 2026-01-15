8d0c05ad5910f11de02f32b0f552de8a351fc980
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-01-15 16:23:17 +0100

    GET request handler

--- app/comment/route.ts
+++ app/comment/route.ts
@@ -1,3 +1,10 @@
+import { comments } from "./data";
+
 export async function GET(){
-    return new Response('comment route');
+    return new Response(JSON.stringify(comments), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
