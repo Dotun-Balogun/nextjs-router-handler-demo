ccf007cd4baee84a883420f8f337c672e146235f
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-01-19 20:58:12 +0100

    added patch request code

--- app/comment/route.ts
+++ app/comment/route.ts
@@ -24,3 +24,21 @@ export  async function POST(request:Request){
    )
 
 }
+
+
+
+
+
+export async function PATCH(
+    request:Request,
+    {params}:{params:Promise<{id:string}>}
+){
+  
+  const {id} = await params;
+  const body = await request.json();
+  const {text} = body
+   const  index = comments.findIndex((comment)=>comment.id === parseInt(id))
+   comments[index].text = text
+    return Response.json(comments[index])
+
+}
