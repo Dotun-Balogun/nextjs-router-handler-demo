38fae936f3fd8aee72f18ea26a71304f8bd1c1e3
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-01-22 16:41:40 +0100

    Header in route hander in profile/api

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,3 +1,13 @@
-export async function GET(){
-    return new Response('user profile')
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request:NextRequest){
+    const reqeustHeader = new Headers(request.headers)
+    console.log(reqeustHeader.get("Authorisation"))
+
+    // using header in next/headers
+    // const headerList =await headers()
+    // console.log(headerList.get("Authorization"))
+
+    return new Response('profile Api data')
 }
