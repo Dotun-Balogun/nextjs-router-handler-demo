014e2b06d55e7bc14bdcc36cfe2b3d526bffb5eb
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-02-03 21:58:10 +0100

    next.js cookies ultility function

--- app/hello/route.ts
+++ app/hello/route.ts
@@ -1,3 +1,40 @@
+import { cookies } from "next/headers";
 export async function GET(){
     return new Response('hello world')
+}
+
+
+
+// nextjs 14 server cookie util functions
+export async function SetServerCookies(
+    name:string,
+    value:string,
+    options: {
+        path?: string;
+        httpOnly?: boolean;
+        secure?: boolean;
+        maxAge?: number;
+        expires?: Date;
+        sameSite?: 'lax' | 'strict' | 'none';
+        domain?: string;
+    } = {}
+){
+    const cookiesStore = await cookies();
+  cookiesStore.set({
+    name,
+    value,
+    ...options
+  }
+  )
+}
+
+
+export async function getServerCookie(name: string) {
+  const cookieStore = await cookies();
+  return cookieStore.get(name)?.value;
+}
+
+export async function deleteServerCookie(name: string, path: string = '/') {
+  const cookieStore = await cookies();
+  cookieStore.delete(name);
 }
